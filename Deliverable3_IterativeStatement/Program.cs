29b8c376afbc0e2a879240dd0cd09977cb5b2428
Enzot <Enzot@DESKTOP-3QSBHAL>
2023-01-31 21:50:32 -0500

    Add project files.

---
+++ Deliverable3_IterativeStatement/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Author:Enzo Tafarello Negrao
+ Date: 1/31/2023
+ Deliverable: Iterative Statement */
+
+using System;
+
+namespace Deliverable3_IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer number between 1 and 100: ");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.Write("Specify the serie by: Even or Odd: ");
+            string series = Console.ReadLine();
+
+
+
+                if (series == "Odd")
+                {
+                    Console.WriteLine("You have selected" + series + "series. The numbers between 0 and " + input + " are: ");
+
+                    for (int i = 1; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "Even")
+                {
+                    Console.WriteLine("You have selected" + series + "series. The numbers between 0 and " + input + " are: ");
+
+                    for (int i = 0; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+        }
+    }
+}
